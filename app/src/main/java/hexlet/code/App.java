304238f05c1f3d5ea73e692506e2c0366d9eaf37
yakov256 <yakov256@yandex.ru>
2023-07-12 15:06:41 +0300

    Project 1; task4 +checkstyle;

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -4,6 +4,5 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Welcome to the Brain Games!");
         Cli.askName();
-
     }
 }
--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -3,11 +3,11 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-    public static void askName(){
+    public static void askName() {
         System.out.println("May I have your name?");
         Scanner scanner = new Scanner(System.in);
         String userName = scanner.next();
-        System.out.println("Hello, " + userName +"!");
+        System.out.println("Hello, " + userName + "!");
 
     }
 }
