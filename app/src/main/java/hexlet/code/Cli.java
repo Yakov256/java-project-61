4990bd63b9ab6452c19a2814878e5f95f20b2229
yakov256 <yakov256@yandex.ru>
2023-07-17 13:40:22 +0300

    Missing Welcome message added

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 
 public class Cli {
     public static String askName() {
-        System.out.println("May I have your name?");
+        System.out.println("Welcome to the Brain Games!");
+        System.out.print("May I have your name? ");
         Scanner scanner = new Scanner(System.in);
         String userName = scanner.next();
         System.out.println("Hello, " + userName + "!");
